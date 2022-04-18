380018eee2f91e79219e4d35967f7d6d9fc29149
LuizLLPz <luizllp321@gmail.com>
2022-04-18 10:40:00 -0300

    Alterando configuração

--- modules/db/Connection.php
+++ modules/db/Connection.php
@@ -3,11 +3,10 @@
         public static function connect($connect_details) {
             try {
                 return new PDO(
-                    $connect_details->tipo,
-                    $connect_details->database,
-                    $connect_details->user,
-                    $connect_details->password,
-                    $connect_details->options
+                    $connect_details['tipo'],
+                    $connect_details['database'],
+                    $connect_details['user'],
+                    $connect_details['password'],
                 );
             } catch (PDOException $e) {
                 echo 'Connection failed: ' . $e->getMessage();
--- modules/init.php
+++ modules/init.php
@@ -2,13 +2,16 @@
     require 'modules/db/Connection.php';
     require 'modules/db/QueryBuilder.php';
     $env = parse_ini_file('./.env');
-    echo var_dump($env);
+    $host = $env['DB_HOST'];
+    $dbname = $env['DB_NAME'];
+    $user = $env['DB_USER'];
+    $password = $env['DB_PASSWORD'];
 
     $connect_details = [
-        'tipo' => "mysql:host={$env['DB_HOST']};",
-        'database' => "dbname={$env['DB_NAME']};",
-        'user' => $env['DB_USER'],
-        'password' => $env['DB_PASS'],
+        'tipo' => "mysql:host={$host};",
+        'database' => "dbname={$dbname};",
+        'user' => $user,
+        'password' => $password,
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ],  
@@ -17,4 +20,5 @@
     $conn = Connection::connect($connect_details);
     $qb = new QueryBuilder($conn);
     $usuarios = $qb->getClient()->prepare('SELECT * FROM usuarios')->execute()->fetchAll();
+    echo "<pre> ".var_dump($usuarios)." </pre>";
 ?>
