9e1c6b653016797b78e0a4744d10e1696cf1f949
Vitor Bianchini de Quadros <vitorbiqua@outlook.com>
2022-04-19 19:27:32 -0300

    Estilo do header

---
+++ client/assets/js/iconFunc.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    const icon = document.getElementById("icon-home")
+    const home = document.getElementById("cabecalho_conteudo")
+    icon.addEventListener('click', ()=> {
+        home.classList.toggle("ativado")
+
+    })
+}
