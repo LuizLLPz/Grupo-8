418585e29e56bfdb6d1191ab8d6659345dfb145e
LuizLLPz <luizllp321@gmail.com>
2022-04-19 17:04:23 -0300

    Reutilizando itens da alteração teste

--- client/components/navbar.php
+++ client/components/navbar.php
@@ -2,8 +2,7 @@
 <?php require STYLES_PATH . 'navbar.css'; ?>
 </style>
 <navbar class="navbar">
-    <a href="" class="navbar__link">Link</a>
-    <a href="" class="navbar__link">Link</a>
+    <a href="/" class="navbar__link">Página inicial</a>
+    <a href="/" class="navbar__link">Fórum</a>
     <a href="/registrar" class="navbar__link">Registrar</a>
-    <a href="" class="navbar__link">Link</a>
 </navbar>
