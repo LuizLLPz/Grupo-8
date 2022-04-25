1a9cc25e306388fc656b4554d43c5f5ac4d9697e
LuizLLPz <luizllp321@gmail.com>
2022-04-25 10:14:29 -0300

    Corrigindo script migração

---
+++ modules/db/migração/1.sql
@@ -0,0 +1,11 @@
+use update_donate;
+create table if not Exists Usuario (
+    id varchar(100) primary key,
+    nome varchar(40),
+    sobrenome varchar(40),
+    email varchar(320),
+    cpf char(14),
+    senha varchar(100),
+    fotoPerfil varchar(320),
+    tipo enum('Doador', 'Donatário');
+);
