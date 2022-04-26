2231821a327b2dd34ce3dec87f1e549928eb7ba6
Vitor Bianchini de Quadros <vitorbiqua@outlook.com>
2022-04-26 18:17:54 -0300

    Alterações no css e add de imagens

--- client/pages/cadastro.php
+++ client/pages/cadastro.php
@@ -6,11 +6,17 @@
             <h1 class="form__heading">Cadastro</h1>
         </div>
         <input type="text" name="nome" placeholder="Nome" required class="form__input">
+        <span class="form_span-normal"><img src=<?=IMAGES_PATH.'icon-erro.svg'?>>Campo inválido - preencha o campo</span>
         <input type="text" name="sobrenome" placeholder="Sobrenome" required class="form__input">
+        <span class="form_span-normal"><img src=<?=IMAGES_PATH.'icon-erro.svg'?>>Campo inválido - preencha o campo</span>
         <input type="email" name="email" placeholder="Email" required class="form__input">
+        <span class="form_span-normal"><img src=<?=IMAGES_PATH.'icon-erro.svg'?>>Campo inválido - preencha o campo</span>
         <input type="text" name="cpf" placeholder="CPF" required class="form__input" id="cpf">
+        <span class="form_span-normal"><img src=<?=IMAGES_PATH.'icon-erro.svg'?>>Campo inválido - preencha o campo</span>
         <input type="password" name="senha" placeholder="Senha" required class="form__input">
+        <span class="form_span-normal"><img src=<?=IMAGES_PATH.'icon-erro.svg'?>>Campo inválido - preencha o campo</span>
         <input type="password" name="passwordConfm" placeholder="Confirmar senha" required class="form__input">
+        <span class="form_span-normal"><img src=<?=IMAGES_PATH.'icon-erro.svg'?>>Campo inválido - preencha o campo</span>
         <div class="center">
             <input type="submit"class="form__button" value="REGISTRAR">
         </div>
