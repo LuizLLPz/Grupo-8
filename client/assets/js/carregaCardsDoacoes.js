5c06f6b14934d96a9cff6e63e304b275f1c70c69
Vitor Bianchini de Quadros <vitorbiqua@outlook.com>
2022-06-06 14:24:01 -0300

    Edição do js e alteração do estilo do card no css

--- client/assets/js/carregaCardsDoacoes.js
+++ client/assets/js/carregaCardsDoacoes.js
@@ -4,32 +4,34 @@ $.ajax({
     type: "GET",
     url: "api/doacao",
     success: function (response) {
+        console.log(response);
         doacoes_cards.innerHTML = ""
 
         response.forEach(element => {
-
+            // Imagem   for example     <img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjNy7KHhM_eAhWGzIMKHUg2CdUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F569098237501098984%2F&psig=AOvVaw2X_qZzZ_X_X_X_XQZz_Z_E&ust=1560795909824000" alt="">
             doacoes_cards.innerHTML += `
             <div class="card">
                 <div class="card_anuncio">
                     <div class="card_anuncio-texto"> 
                         <h1 class="texto_card titulo">${element.titulo}</h1>
                         <h3 class="texto_card categoria">${element.categoria}</h3>
-                        <h3 class="texto_card data">${element.data}</h3>
+                        <h3 class="texto_card data">${element.data_publicacao}</h3>
                         <p class="descricao">${element.descricao}</p>
                     </div>
                     
                     <div class="card_anuncio-img">
                         <div class="img-card">
-                        <div class="fundo_perfil">
-                            <div class="foto_perfil_doacao">
-                                <img src="foto_perfil" src="images/perfil.jpg">
+                            <div class="fundo_perfil">
+                                <div class="foto_perfil_doacao">
+                                    <img src="foto_perfil" src="".jpg"> 
+                                </div>
                             </div>
                         </div>
                         <div class="azul">
-                            <p class="texto_doacao nome_usuario">Usuario</p>
-                            <p class="texto_doacao avaliacao">Avaliação</p>
+                            <p class="texto_doacao nome_usuario">${element.usuario}</p>
+                            <p class="texto_doacao denunciar">Denunciar</p>
                             <div class="foto_anuncio_daocao">
-                                <img class="imagem_doacao" src="${element.imagem}">
+                                <img class="imagem_doacao" src="https://picsum.photos/300/300">
                             </div>
                         </div>
                     </div>
