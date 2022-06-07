c0ce3bef0f16e869d6a46d1b0d7d56911afdc528
Vitor Bianchini de Quadros <vitorbiqua@outlook.com>
2022-06-07 17:47:31 -0300

    Alteração no filtro

--- client/assets/js/carregaCardsDoacoes.js
+++ client/assets/js/carregaCardsDoacoes.js
@@ -4,9 +4,9 @@ $.ajax({
     type: "GET",
     url: "api/doacao",
     success: function (response) {
-        console.log(response);
         doacoes_cards.innerHTML = ""
 
+        let contador = 0;
         response.forEach(element => {
             // Imagem   for example     <img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjNy7KHhM_eAhWGzIMKHUg2CdUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F569098237501098984%2F&psig=AOvVaw2X_qZzZ_X_X_X_XQZz_Z_E&ust=1560795909824000" alt="">
             doacoes_cards.innerHTML += `
@@ -14,9 +14,11 @@ $.ajax({
                 <div class="card_anuncio">
                     <div class="card_anuncio-texto"> 
                         <h1 class="texto_card titulo">${element.titulo}</h1>
-                        <h3 class="texto_card categoria">Categotia</h3>
+                        <h3 class="texto_card categoria">Categotia ${contador++}</h3>
                         <h3 class="texto_card data">${element.data_publicacao}</h3>
                         <p class="descricao">${element.descricao}</p>
+                        <p class="texto_card estado">Estado: EsttadoX</p>
+                        <p class="texto_card cidade">Cidade: CidadeX</p>
                     </div>
                     
                     <div class="card_anuncio-img">
--- client/assets/js/filtroDoacoes.js
+++ client/assets/js/filtroDoacoes.js
@@ -5,15 +5,18 @@ const img = document.getElementById("img");
 
 img.addEventListener('click', () => { console.log(img)});
 
-let doacao_select = document.getElementById('doacao_select');
-doacao_select = doacao_select.options[doacao_select.selectedIndex].value;
-console.log(doacao_select);
 
 doacao_input.addEventListener('input', e => {
     const pesquisa = e.target.value.trim().toLowerCase();
+    
+    const listaCrds = Array.from(doacoes_cards.children);
+    
+    let doacao_select = document.getElementById('doacao_select');
+    doacao_select = doacao_select.options[doacao_select.selectedIndex].value;
+    
     switch (doacao_select) {
         case "sem" :
-            Array.from(doacoes_cards.children).filter(element => {
+            listaCrds.filter(element => {
                 
                 if (element.children[0].children[0].children[0].textContent.toLowerCase().includes(pesquisa)) {
                     element.style.display = "block";
@@ -23,9 +26,26 @@ doacao_input.addEventListener('input', e => {
             });
             break;
         case 'categoria':
-            Array.from(doacoes_cards.children).filter(element => {
-        
+            listaCrds.filter(element => {
                 if (element.children[0].children[0].children[1].textContent.toLowerCase().includes(pesquisa)) {
+                    element.style.display = "block";
+                } else {
+                    element.style.display = "none";
+                }
+            });
+            break;
+        case 'est':
+            listaCrds.filter(element => {
+                if (element.children[0].children[0].children[4].textContent.toLowerCase().includes(pesquisa)) {
+                    element.style.display = "block";
+                } else {
+                    element.style.display = "none";
+                }
+            });
+            break;
+        case 'cit':
+            listaCrds.filter(element => {
+                if (element.children[0].children[0].children[5].textContent.toLowerCase().includes(pesquisa)) {
                     element.style.display = "block";
                 } else {
                     element.style.display = "none";
