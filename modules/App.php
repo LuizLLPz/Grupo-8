394945b9ebe19b559debdd2eeb5d41a8992f6ce8
LuizLLPz <luizllp321@gmail.com>
2022-06-07 19:56:36 -0300

    Melhorias ao servir dados de requisição a API

--- modules/App.php
+++ modules/App.php
@@ -3,6 +3,9 @@ class App {
     public static function apiServe() {
         header ("Access-Control-Allow-Origin: *");
         $data = !empty($_POST) ? $_POST : json_decode(file_get_contents('php://input'), true);
+        if (!$data) {
+            $data = ($_GET);
+        }
         return $data;
     }
 
@@ -18,7 +21,6 @@ class App {
         if($changeDoc) {
             header('Content-Type: text/html');
         }
-        //check if response header is defined to json
         if((!isset($_SERVER['HTTP_ACCEPT']) || $_SERVER['HTTP_ACCEPT'] != 'application/json') && $changeDoc) {
             $res = '<pre> '. print_r($data, true) . '</pre>';
         } else {
